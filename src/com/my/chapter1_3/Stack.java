80b8e5d10d7e80b5d4349c994748641aaab0dc8d
zsxneil <zsxneil@hotmail.com>
2017-10-14 18:11:46 +0800

    栈的实现例子

---
+++ src/com/my/chapter1_3/Stack.java
@@ -0,0 +1,68 @@
+package com.my.chapter1_3;
+
+import java.util.Iterator;
+
+/**
+ * 栈的链表实现
+ * Created by Administrator on 2017/10/14.
+ */
+public class Stack<Item> implements Iterable<Item>{
+    private Node first; //栈顶
+    private int N;      //元素数量
+
+    private class Node{
+        //定义了结点的嵌套类
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        @Override
+        public void remove() {
+
+        }
+    }
+}
